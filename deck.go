3b3c7025f98d7fc946ae849b9f6f1650779c14fd
Daniel <argonianseller@gmail.com>
2023-04-30 15:08:40 +0300

    + generating full 52 cards deck func

--- deck.go
+++ deck.go
@@ -4,8 +4,23 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	newCards := deck{}
+
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	cardSuits := []string{"Diamonds", "Hearts", "Clubs", "Spades"}
+
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			newCards = append(newCards, cardValue+" of "+cardSuit)
+		}
+	}
+
+	return newCards
+}
+
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i+1, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- main.go
+++ main.go
@@ -3,12 +3,6 @@ package main
 var num int
 
 func main() {
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Ace of Diamonds")
-
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
