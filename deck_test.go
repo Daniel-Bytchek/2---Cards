8878c69eb6cd2ca978d0098a7d454606f431d7f1
Daniel <argonianseller@gmail.com>
2023-05-05 17:28:33 +0300

    added more newDeck tests

--- deck_test.go
+++ deck_test.go
@@ -10,3 +10,19 @@ func TestNewDeck(t *testing.T) {
 	}
 
 }
+
+func TestFirstCard(t *testing.T) {
+	d := newDeck()
+	first_card := "Ace of Diamonds"
+	if d[0] != first_card {
+		t.Errorf("Expected first card %v, not %v", first_card, d[0])
+	}
+}
+
+func TestLastCard(t *testing.T) {
+	d := newDeck()
+	last_card := "King of Spades"
+	if d[len(d)-1] != last_card {
+		t.Errorf("Expected last card %v, not %v", last_card, d[len(d)-1])
+	}
+}
